351548ecd53df1945e5b92880f4a1e51100d1b54
ZhenShen-1 <e_afanasev@mail.ru>
2023-05-17 21:59:27 +0500

    Change int to long in Main, SalesManager

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void  main(String[] args) {
-        int [] sales = new int[] {10, 200, 3000, 40000};
+        long [] sales = new long[] {10, 200, 3000, 40000};
         SalesManager salesManager = new SalesManager(sales);
 
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
 
     }
